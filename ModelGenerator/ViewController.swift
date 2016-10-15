991fd396b64685ec9594fd9ea4b65846d9679483
Sheepy <sheepyyoung@foxmail.com>
2016-10-15 23:41:19 +0800

    Optimize code structure

--- ModelGenerator/ViewController.swift
+++ ModelGenerator/ViewController.swift
@@ -66,55 +66,72 @@ private extension ViewController {
         modelExtension += "\(indentation)mutating func mapping(map: Map) {\n"
 
         dict.forEach {
-            var key = $0.key
-            if key.contains(underline) {
-                let words = key.components(separatedBy: underline)
-                key = ""
-                words.forEach { key += $0.capitalized }
-            }
-
+            let variableName = normalizeVariableName(key: $0.key)
             var value = $0.value
-            // String
-            if value is String {
-                value = "\"\""
-            }
-            // TODO: Number(contains bool), NSNull
-            if let number = value as? NSNumber, number.isBool {
-                value = number.boolValue
-            }
-            // Object
-            if let dictValue = value as? [String: Any] {
-                let model = key.capitalized
-                nestedModel += "\n\n// MARK: - \(model)\n\n\(convert(dictValue, to: model))"
+
+            switch value {
+            case is String:
+
+                modelDefine += "\(indentation)var \(variableName) = \"\"\n"
+
+            case let number as NSNumber:
+
+                if number.isBool {
+                    modelDefine += "\(indentation)var \(variableName) = false\n"
+                } else {
+                    modelDefine += "\(indentation)var \(variableName) = 0\n"
+                }
+
+            case let dict as [String: Any]:
+
+                let model = variableName.capitalized
+                nestedModel += "\n\n// MARK: - \(model)\n\n\(convert(dict, to: model))"
                 // Declare an optional object
-                modelDefine += "\(indentation)var \(key): \(model)?\n"
-            } else if let array = value as? [[String: Any]] {
-                // Array
-                var model = key
-                if key.contains(listFlag) {
-                    model = key.replacingOccurrences(of: listFlag, with: "")
-                }
-                if String(key.characters.last!) == pluralFlag {
-                    model = String(key.characters.dropLast())
-                }
-                model = model.capitalized
+                modelDefine += "\(indentation)var \(variableName): \(model)?\n"
+
+            case let array as [[String: Any]]:
+
+                let model = normalizeArrayElement(variableName: variableName)
                 value = "[\(model)]()\n"
                 if !array.isEmpty {
                     nestedModel += "\n\n// MARK: - \(model)\n\n\(convert(array.first, to: model))"
                 }
-                modelDefine += "\(indentation)var \(key): [\(model)] = []\n"
-            } else {
-                // Declare with initial value
-                modelDefine += "\(indentation)var \(key) = \(value)\n"
+                // Declare an array
+                modelDefine += "\(indentation)var \(variableName): [\(model)] = []\n"
+
+            default:
+                // Declare variable with initial value
+                modelDefine += "\(indentation)var \(variableName) = \(value)\n"
             }
 
-            modelExtension += "\(indentation)\(indentation)\(key) <- map[\"\(key)\"]\n"
+            modelExtension += "\(indentation)\(indentation)\(variableName) <- map[\"\($0.key)\"]\n"
         }
 
         modelDefine += "\n\(initFunc)\n}"
         modelExtension += "\(indentation)}\n}"
         result += "\(modelDefine)\n\n\(modelExtension)\(nestedModel)"
         return result
+    }
+
+    func normalizeVariableName(key: String) -> String {
+        var name = key
+        if name.contains(underline) {
+            var words = name.components(separatedBy: underline)
+            name = words.removeFirst()
+            words.forEach { name += $0.capitalized }
+        }
+        return name
+    }
+
+    func normalizeArrayElement(variableName: String) -> String {
+        var model = variableName
+        if variableName.contains(listFlag) {
+            model = variableName.replacingOccurrences(of: listFlag, with: "")
+        }
+        if String(variableName.characters.last!) == pluralFlag {
+            model = String(variableName.characters.dropLast())
+        }
+        return model.capitalized
     }
 }
 
